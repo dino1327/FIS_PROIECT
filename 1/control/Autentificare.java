745d7c57f0726dc1f16191b1e01a2bd45b2c4549
dino1327 <102035515+dino1327@users.noreply.github.com>
2022-05-23 13:57:24 +0300

    Add files via upload

---
+++ 1/control/Autentificare.java
@@ -0,0 +1,114 @@
+package org.fis2022.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import org.fis2022.exceptions.InvalidCredentials;
+import org.fis2022.models.User;
+import org.fis2022.services.UserService;
+
+import java.io.IOException;
+
+public class Autentificare {
+
+    @FXML
+    public Text loginMessage;
+    @FXML
+    public PasswordField passwordField;
+    @FXML
+    public TextField usernameField;
+
+    @FXML
+    public void handleLoginAction() {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        if (username == null || username.isEmpty()) {
+            loginMessage.setText("Adauga username!");
+            return;
+        }
+
+        if (password == null || password.isEmpty()) {
+            loginMessage.setText("Adauga parola!");
+            return;
+        }
+
+        String encoded_password = UserService.encodePassword(username, password);
+
+        try{
+            String stored_password = UserService.getHashedUserPassword(username);
+            if(stored_password.equals(encoded_password)){
+                if(UserService.getUser(username).getRole().equals("Client")){
+                    loadHomePage();
+                    return;
+                }
+                if(UserService.getUser(username).getRole().equals("Agent")){
+                    loadAgentHomePage();
+                    return;
+                }
+                loginMessage.setText("nope");
+                return;
+            }
+
+        } catch(InvalidCredentials e){
+            loginMessage.setText(e.getMessage());
+            return;
+        }
+
+        loginMessage.setText("Autentificare incorecta!");
+    }
+
+    @FXML
+    public void loadRegisterPage(){
+        try {
+            Stage stage = (Stage) loginMessage.getScene().getWindow();
+            Parent registerRoot = FXMLLoader.load(getClass().getResource("/fxml/register.fxml"));
+            Scene scene = new Scene(registerRoot, 640, 480);
+            stage.setTitle("Inregistrare - Real Estate App");
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void loadHomePage(){
+        try{
+            User u = UserService.getUser(usernameField.getText());
+            Stage stage = (Stage) loginMessage.getScene().getWindow();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/PaginaListari.fxml"));
+            stage.setUserData(u);
+            Parent homeRoot = loader.load();
+            Scene scene = new Scene(homeRoot, 640, 480);
+            stage.setTitle("Real Estate App");
+            stage.setScene(scene);
+        } catch (InvalidCredentials e){
+            loginMessage.setText(e.getMessage());
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void loadAgentHomePage(){
+        try{
+            User u = UserService.getUser(usernameField.getText());
+            Stage stage = (Stage) loginMessage.getScene().getWindow();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/AgentHomePage.fxml"));
+            stage.setUserData(u);
+            Parent homeRoot = loader.load();
+            Scene scene = new Scene(homeRoot, 640, 480);
+            stage.setTitle("Real Estate App");
+            stage.setScene(scene);
+        } catch (InvalidCredentials e){
+            loginMessage.setText(e.getMessage());
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
